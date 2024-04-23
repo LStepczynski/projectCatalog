6b917d5d118ba2df1f49dd75e610c6866911c1a1
LStepczynski <leonstepczynski@gmail.com>
2024-04-22 19:58:33 -0500

    feat: added new routes

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,19 +1,28 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Index } from "./pages/index";
+import { Account } from "./pages/account";
+import { Categories } from "./pages/categories";
+import { Category } from "./pages/category";
 
 export const Routes = () => {
-
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <p>/</p>,
+      element: <Index />,
+    },
+    {
+      path: "/categories",
+      element: <Categories />,
+    },
+    {
+      path: "/account",
+      element: <Account />,
+    },
+    {
+      path: "/categories/:categoryName",
+      element: <Category />,
     },
   ]);
 
-  return (
-    <RouterProvider router={router} />
-  );
-}
-
+  return <RouterProvider router={router} />;
+};
