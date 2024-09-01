96413d1fb782d42ebeac43d5a90b87a116a290d1
LStepczynski <leonstepczynski@gmail.com>
2024-09-01 19:15:26 +0200

    when changing a profile picture it will update on every article the user owns

--- backend/src/routes/v1/userapi.ts
+++ backend/src/routes/v1/userapi.ts
@@ -74,48 +74,8 @@ router.post(
       });
     }
 
-    let user = await UserManagment.getUser(Username);
-
-    if (!user) {
-      return res
-        .status(404)
-        .send({ status: 404, response: { message: 'user not found' } });
-    }
-
-    const oldImageId = user.ProfilePic.match(
-      /images\/([a-f0-9-]+)\.(?:png|jpg|jpeg|gif)$/
-    );
-    if (oldImageId) {
-      S3.removeImageFromS3(oldImageId[1]);
-    }
-
-    const imageId = uuidv4();
-
-    user.ProfilePic = `${process.env.AWS_S3_LINK}/images/${imageId}.png`;
-
-    try {
-      const response = await S3.saveImage(imageId, req.file, 350, 350);
-      if (!response) {
-        throw new Error('S3 error');
-      }
-    } catch (err) {
-      console.log('Error: ', err);
-      return res.status(500).send({
-        status: 500,
-        response: { message: 'server error' },
-      });
-    }
-
-    const result = await UserManagment.updateUser(
-      Username,
-      'ProfilePic',
-      user.ProfilePic
-    );
-
-    const resultWithToken: any = result;
-    delete user.Password;
-    resultWithToken.response.verificationToken = UserManagment.getNewJWT(user);
-    return res.status(resultWithToken.status).send(resultWithToken);
+    const response = await UserManagment.changeProfilePic(Username, req.file)
+    return res.status(response.status).send(response);
   }
 );
 
