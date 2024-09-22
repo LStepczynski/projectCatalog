f501bce4688114c262319be89677c9255270294e
LStepczynski <leonstepczynski@gmail.com>
2024-09-22 03:37:58 +0200

    added an Email class that uses nodemailer to send emails

---
+++ backend/src/services/Email.ts
@@ -0,0 +1,38 @@
+import nodemailer from 'nodemailer'
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+export class Email {
+  private static EMAIL_PASSWORD = process.env.MAIL_PASSWORD
+  private static EMAIL_ADDRESS = process.env.MAIL_ADDRESS
+  
+  public static sendEmail(recipient: string, subject: string, body: string): Boolean {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: this.EMAIL_ADDRESS,
+        pass: this.EMAIL_PASSWORD
+      }
+    });
+    
+    // Email options
+    const mailOptions = {
+      from: `Course Catalog <${this.EMAIL_ADDRESS}>`, 
+      to: recipient,
+      subject: subject,
+      text: body,
+    };
+    
+    // Send the email
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Error sending email:', error);
+        return false 
+      }
+      console.log('Email sent:', info.response);
+      return true
+    });
+    return false
+  } 
+}
