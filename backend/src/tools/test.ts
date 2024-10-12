61ff2a57595655157547eaa8c80f32e0f0ae847b
LStepczynski <leonstepczynski@gmail.com>
2024-10-12 23:12:37 +0200

    config changes

--- backend/src/tools/test.ts
+++
@@ -1,5 +0,0 @@
-import { UserManagment } from "../services/userManagment";
-import { Articles } from "../services/articles";
-
-console.log(UserManagment.updateUser("Lodor", "Admin", 'true'))
-// Articles.removeArticle('ArticlesUnpublished', "ebe11b5c-0be3-4bd1-96ed-a6b510fbc56f")
