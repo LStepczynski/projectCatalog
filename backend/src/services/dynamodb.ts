95064987927fcb1c5cc7ed611f4f9135a5276b0b
LStepczynski <leonstepczynski@gmail.com>
2024-10-12 23:11:30 +0200

    changed the client to work differently depending on the stage

--- backend/src/services/dynamodb.ts
+++ backend/src/services/dynamodb.ts
@@ -4,8 +4,10 @@ dotenv.config();
 
 export const client = new DynamoDB({
   region: 'us-east-2',
-  // credentials: {
-  //   accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
-  //   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
-  // },
+  ...(process.env.STATE === 'DEV' && {
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+    },
+  }),
 });
