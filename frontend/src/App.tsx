57d763602c289704e4d6d00442cc4c7f4b82724e
LStepczynski <leonstepczynski@gmail.com>
2024-11-13 18:03:29 -0600

    renamed files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider, BaseStyles, Box } from '@primer/react';
 import { Routes } from './routes';
 import { NavBar } from './components/layout/navBar';
-import { EmailVerificationPopup } from './components/common/emailVerificationPopup';
+import { EmailVerificationPopup } from '@components/common/popups/emailVerificationPopup';
 
 export const App = () => {
   const mode = 'dark';
