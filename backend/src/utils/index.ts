36e5ac476bae6338f59454088c8561646fbf8185
LStepczynski <leonstepczynski@gmail.com>
2024-12-08 13:04:25 -0600

    added the role and authenticate middleware

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -11,9 +11,9 @@ export { verifyToken } from '@utils/jwt/verifyToken';
 export { validatePassword } from '@utils/validations/validatePassword';
 export { checkUniqueUser } from '@utils/validations/checkUniqueUser';
 
+export { errorHandler, role, authenticate } from '@utils/middleware';
 export { isValidString } from '@utils/validations/isValidString';
 export { isValidEmail } from '@utils/validations/isValidEmail';
 export { getUnixTimestamp } from '@utils/getUnixTimestamp';
 export { asyncHandler } from '@utils/asyncHandler';
-export { errorHandler } from '@utils/middleware';
 export { resizeImage } from '@utils/resizeImage';
