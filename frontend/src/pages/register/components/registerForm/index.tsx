3e0fbe070c11e0fd3ed7f84eedfb06b323b54738
LStepczynski <leonstepczynski@gmail.com>
2025-01-08 21:35:06 -0600

    refactored the register page

---
+++ frontend/src/pages/register/components/registerForm/index.tsx
@@ -0,0 +1,104 @@
+import React, { useState, ChangeEvent } from 'react';
+
+import { Box, TextInput, Text } from '@primer/react';
+import {
+  PersonIcon,
+  LockIcon,
+  MailIcon,
+  EyeIcon,
+  EyeClosedIcon,
+} from '@primer/octicons-react';
+
+interface FormData {
+  username: string;
+  password: string;
+  email: string;
+}
+
+interface Props {
+  setFormData: React.Dispatch<React.SetStateAction<FormData>>;
+  formData: FormData;
+}
+
+export const RegisterForm = (props: Props) => {
+  const { setFormData, formData } = props;
+
+  const [passwordVisible, setPasswordVisible] = useState<boolean>(false);
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible((prev) => !prev);
+  };
+
+  const inputStyle = {
+    width: '300px',
+  };
+
+  return (
+    <>
+      {/* Username input */}
+      <Box sx={{ display: 'grid' }}>
+        <Text sx={{ color: 'fg.muted' }}>Username:</Text>
+        <TextInput
+          aria-label="Username"
+          name="username"
+          size="large"
+          placeholder="Username"
+          value={formData.username}
+          onChange={handleInputChange}
+          leadingVisual={PersonIcon}
+          sx={inputStyle}
+          required
+        />
+      </Box>
+
+      {/* Email input */}
+      <Box sx={{ display: 'grid' }}>
+        <Text sx={{ color: 'fg.muted' }}>Email:</Text>
+        <TextInput
+          aria-label="Email"
+          name="email"
+          size="large"
+          placeholder="Email"
+          type="email"
+          value={formData.email}
+          onChange={handleInputChange}
+          leadingVisual={MailIcon}
+          sx={inputStyle}
+          required
+        />
+      </Box>
+
+      {/* Password input */}
+      <Box sx={{ display: 'grid' }}>
+        <Text sx={{ color: 'fg.muted' }}>Password:</Text>
+        <TextInput
+          aria-label="Password"
+          name="password"
+          type={passwordVisible ? 'text' : 'password'}
+          size="large"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleInputChange}
+          leadingVisual={LockIcon}
+          trailingAction={
+            <TextInput.Action
+              onClick={togglePasswordVisibility}
+              icon={passwordVisible ? EyeClosedIcon : EyeIcon}
+              aria-label={passwordVisible ? 'Hide password' : 'Show password'}
+            />
+          }
+          sx={inputStyle}
+          required
+        />
+      </Box>
+    </>
+  );
+};
---
+++ frontend/src/pages/register/hooks/useFormSubmit.ts
@@ -0,0 +1,99 @@
+import { useState, FormEvent } from 'react';
+
+import { capitalize } from '@utils/capitalize';
+import { fetchWrapper } from '@utils/fetchWrapper';
+
+interface FormData {
+  username: string;
+  password: string;
+  email: string;
+}
+
+export const useFormSubmit = () => {
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+  const [formData, setFormData] = useState<FormData>({
+    username: '',
+    password: '',
+    email: '',
+  });
+
+  /**
+   * Validates the form data.
+   *
+   * @returns {boolean} - Returns true if the form data is valid, otherwise false.
+   */
+  const validateForm = (): boolean => {
+    const { username, password, email } = formData;
+
+    if (
+      username.trim() === '' ||
+      password.trim() === '' ||
+      email.trim() === ''
+    ) {
+      setErrorMessage('All fields must be filled.');
+      return false;
+    }
+
+    if (password.length < 8) {
+      setErrorMessage('Password must be at least 8 characters long.');
+      return false;
+    }
+
+    // Simple email regex for validation
+    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (!emailRegex.test(email)) {
+      setErrorMessage('Invalid email address.');
+      return false;
+    }
+
+    setErrorMessage('');
+    return true;
+  };
+
+  /**
+   * Handles the form submission event.
+   *
+   * @param {FormEvent} event - The form submission event.
+   * @returns {Promise<void>} A promise that resolves when the form submission is complete.
+   *
+   * This function prevents the default form submission behavior, validates the form,
+   * sets the loading state, and attempts to submit the form data to the backend.
+   * If the response status is not 200, it sets an error message. If an error occurs
+   * during the fetch operation, it logs the error and sets an error message.
+   * Finally, it resets the loading state.
+   */
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    if (!validateForm()) return;
+
+    setLoading(true);
+    try {
+      const backendUrl = import.meta.env.VITE_BACKEND_URL;
+      const response = await fetchWrapper(`${backendUrl}/user/sign-up`, {
+        method: 'POST',
+        body: JSON.stringify(formData),
+      });
+
+      if (response.status !== 200) {
+        setErrorMessage(capitalize(response.response.message) + '.');
+        return;
+      }
+
+      window.location.href = '/sign-in';
+      setFormData({
+        username: '',
+        password: '',
+        email: '',
+      });
+    } catch (err: any) {
+      console.error('Registration error:', err);
+      setErrorMessage(capitalize(err.message || 'An error occurred') + '.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { errorMessage, loading, setFormData, formData, handleSubmit };
+};
