070e40324aebaf667f232b482325fba391bbe5b3
LStepczynski <leonstepczynski@gmail.com>
2025-01-28 16:30:37 -0600

    deleted the old src files

--- backend/src_old/services/dynamodb.ts
+++
@@ -1,7 +0,0 @@
-import { DynamoDB } from '@aws-sdk/client-dynamodb';
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const client = new DynamoDB({
-  region: process.env.STATE === 'PRODUCTION' ? 'us-east-2' : 'local',
-});
